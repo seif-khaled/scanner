9558b2999cbc295db6ecda5bc20f00031bfc9b9c
Seif Khaled <seifkhaled2424@gmail.com>
2022-11-25 10:53:11 +0200

    added some changes

---
+++ comp.py
@@ -0,0 +1,21 @@
+import re
+import tabulate as tbl
+import os,sys
+
+
+class scanner:
+    def __init__(self):
+        pass
+    def idnetifier_chekcer(self):
+        pass
+    def reserved_words_chekcer(self):
+        
+    def operator_parnthies_chekcer(self):
+        pass
+    def comments_chekcer(self):
+        pass
+    
+identifier=0
+if identifier==r"[0-9]":
+    print("true")
+# print("hello shinfa")
