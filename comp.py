539e3303747cc3b047d4639c31f54fb3e93a7e80
Seif Khaled <seifkhaled2424@gmail.com>
2022-11-25 14:04:45 +0200

    added stuff

--- comp.py
+++ comp.py
@@ -4,18 +4,54 @@ import os,sys
 
 
 class scanner:
-    def __init__(self):
-        pass
+    def __init__(self,text):
+        self.text=text
+        self.text=self.text.split()
     def idnetifier_chekcer(self):
         pass
     def reserved_words_chekcer(self):
+        # skipper=0
+        switch=0
+        pattern="(Read|Write|Begin|End|Declare|INT|FLOAT|CHAR|STRING|Set|IF|Then|Else|ENDIF|While|DO|ENDWHILE|Until|ENDUNTIL|Call)"
+        for i in range(len(self.text)):
+            # if(skipper!=0 and switch!=0):
+            #     skipper-=1
+            #     continue
+            if(self.text[i]=='{'):
+                switch=1
+                # skipper+=1
+                continue
+            if(switch==1 and self.text[i]!='}' ):
+                continue
+            if(self.text[i]=='}'):
+                switch=0
+                continue
+            else:
+                for i in range(len(self.text)):
+                    res=re.match(pattern,self.text)
+                    if self.text[i]
+
+
+                
+            # else:
+                
+
+
+
         
     def operator_parnthies_chekcer(self):
         pass
     def comments_chekcer(self):
         pass
-    
-identifier=0
-if identifier==r"[0-9]":
-    print("true")
+pattern="(Read|Write|if|else)"
+text="Read if"
+res=re.match(pattern,text)
+if res:
+    print("yes")
+else:
+    print("no")
+
+# print()
+# if identifier==r"[0-9]":
+#     print("true")
 # print("hello shinfa")
