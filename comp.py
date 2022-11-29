5c9be91508f4c6f1bf575d449169a375319f48a7
Seif Khaled <seifkhaled2424@gmail.com>
2022-11-29 17:42:47 +0200

    finished

--- comp.py
+++ comp.py
@@ -1,5 +1,5 @@
 import re
-import tabulate as tbl
+from tabulate import tabulate as tbl
 import os,sys
 
 
@@ -15,11 +15,14 @@ class scanner:
             self.items[self.dict_keys[i]]=[]        
             
     def idnetifier_const_chekcer(self):
+        satsified=0
         switch=0
-        pattern_ident="_?[a-zA-Z_]+[0-9a-zA-Z_]*"
+        pattern_ident="[a-zA-Z_][a-zA-Z0-9_]*"
         pattern_int_const="-?[0-9]+"
         pattern_float_const="-?\d+\.\d+"
         for i in range(len(self.text)):
+            chekcer=re.match(self.patternkw,self.text[i])
+            # print(chekcer)
             if(self.text[i]=='{'):
                 switch=1
                 # skipper+=1
@@ -30,27 +33,27 @@ class scanner:
                 switch=0
                 continue
             else:
-                if self.text[i] not in self.patternkw and self.text[i] not in self.patternoperators :
+                if (chekcer==None) and (self.text[i] not in self.patternoperators) :
+                    # res=is_match_idenetifier(self.text[i])
+                    # print(res)
                     res=re.match(pattern_ident,self.text[i])
                     res2=re.match(pattern_int_const,self.text[i])
                     res3=re.match(pattern_float_const,self.text[i])
-                    r1=re.findall(pattern_ident,self.text[i])
-                    r2=re.findall(pattern_int_const,self.text[i])
-                    r3=re.findall(pattern_float_const,self.text[i])
-                    # print(r1,"\n",r2,"\n",r3,"\n")
+                
                     if res:
                         self.items["identifier"].append(self.text[i])
                         self.items["identifier"].append(i)
-                    if res2:
+                    elif res2:
                         # print("iam a constant")
                         self.items["constant"].append(self.text[i])
                         self.items["constant"].append(i)
-                    if res3:
+                    elif res3:
                          self.items["constant"].append(self.text[i])
                          self.items["constant"].append(i)
                     else:
                         continue
     def reserved_words_chekcer(self):
+        satsified=0
         switch=0
         for i in range(len(self.text)):
             if(self.text[i]=='{'):
@@ -71,6 +74,7 @@ class scanner:
                     continue
 
     def operator_parnthies_semicol_chekcer(self):
+        satsified=0
         switch=0
         # pattern="(\(|\)|=|+|-|*|/|>|<|!)"
 
@@ -152,20 +156,60 @@ class scanner:
         self.operator_parnthies_semicol_chekcer()
         self.idnetifier_const_chekcer()
 
-                    # self.items[pattern[key_item_index]][pattern[key_item_index]]=
-                # res=re.match(pattern,self.text[i])
-                # if res:
 
-# pattern=""
-# pattern="(Read|Write|Begin|End|Declare|INT|FLOAT|CHAR|STRING|Set|IF|Then|Else|ENDIF|While|DO|ENDWHILE|Until|ENDUNTIL|Call)"
-# text="ReadRead what he has Write and Begin Declarew . in The End , Call your father Else he will hit you Untill you are dead"
-
-# text="125 1588 132 -1355 -1 -56 2.3"
-
-# validatign text before excuting the scanner
 ###################################################
+def table_list(dictionary,words,actual_number_of_lexmes,num_of_lex):
+    Index=0
+    final_table=[]
+    switch=0
+    indcies_to_skip=[]
+    index_toput_into=0
+    for i in range(actual_number_of_lexmes):
+        final_table.append([])
+    #loop on each key and get the index of each items
+   
+    counter=0
+    for i in range(len(words)):
+        if(words[i]=='{'):
+            switch=1
+            indcies_to_skip.append(i)
+            continue
+        if(switch==1 and words[i]!='}' ):
+            indcies_to_skip.append(i)
+            continue
+        if(words[i]=='}'):
+            indcies_to_skip.append(i)
+            switch=0
+            continue
+    print(indcies_to_skip)
+    flag=0
+    while(counter!=actual_number_of_lexmes and flag!=1 ):
+        if Index==num_of_lex:
+            break
+        else:
+            for i in dictionary: 
+                if Index==num_of_lex:
+                    flag=1
+                    break
+                elif Index in dictionary[i] and Index not in indcies_to_skip:
+                    index_of_current_lexem=(dictionary[i].index(Index))-1
+                    current_lex=dictionary[i][index_of_current_lexem]
+                    final_table[index_toput_into].append(current_lex)
+                    final_table[index_toput_into].append(i)
+                    Index+=1
+                    counter+=1
+                    index_toput_into+=1
+                    break
+                elif Index in indcies_to_skip:
+                    Index=(indcies_to_skip.index(Index))+1
+           
+    return final_table
+            
+#######################################
 flag=0
 while(flag==0):
+    switch=0
+    list_to_work_on=[]
     number_of_lexmes=eval(input("enter the number of lexmes you will input:"))
     print("\n")
     text=""
@@ -173,10 +217,31 @@ while(flag==0):
         lexme=input("enter a lexeme word:")
         text+=lexme
         text+=" "
+        list_to_work_on.append(lexme)
+    index_to_start_at=1
+    new_list_to_work_on=[]
+    LIST=text.split()
+    for j in range(len(list_to_work_on)):
+        if(list_to_work_on[j]=='{'):
+            switch=1
+            index_to_start_at+=1
+            continue
+        if(switch==1 and list_to_work_on[j]!='}' ):
+            index_to_start_at+=1
+            continue
+        if(list_to_work_on[j]=='}'):
+            switch=0
+            index_to_start_at+=1
+            continue
+        else:
+            new_list_to_work_on.append(list_to_work_on[j])
+    print(new_list_to_work_on)        
     x=scanner(text)
     x.executer()
-    print(x.items)
     print("\n")
+    DATA=table_list(x.items,LIST,len(new_list_to_work_on),number_of_lexmes)
+    print(table_list(x.items,LIST,len(new_list_to_work_on),number_of_lexmes))
+    print(tbl(DATA,headers=["lexeme","Token"],tablefmt='fancy_grid'))
     choice=input("do you want to continue?")
     if choice=='yes':
         os.system('cls||clear')
@@ -184,31 +249,3 @@ while(flag==0):
     elif choice=='no':
         flag=1
 
-#####################################
-# dictionary={'key1':[]}
-# dictionary["key1"].append("shinfa")
-# dictionary["key1"].append(1)
-
-# print(dictionary["key1"])
-# text="_X _X3 x2 X1 x33for valid_identifier_ nothim3 3x1 text_search"
-# text=text.split()
-# pattern="-?\d+\.\d+"
-# pattern="_?[a-zA-Z_]+[0-9a-zA-Z_]*"
-# print(text)
-# for i in range(len(text)):
-#     res=re.match(pattern,text[i])
-#     if res:
-#         print("yes",text[i])
-#     else:
-#         print("no",text[i])
-# res=re.findall(pat,text)
-# print(res)
-# if res:
-#     print("yes")
-# else:
-#     print("no")
-
-# print()
-# if identifier==r"[0-9]":
-#     print("true")
-# print("hello shinfa")
